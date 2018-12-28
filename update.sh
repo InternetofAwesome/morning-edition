4effdc142c7e1e12fd504b078301db525ee0ae9c
Sam Anderson <sam@pax.com>
2018-12-28 09:38:37 -0800

    add update script

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+pip_packages="lxml"
+
+mkdir -p package
+cd package 
+pip install $pip_packages --target .
+zip -r9 ../function.zip .
+cd ..
+zip -g function.zip function.py
+
+aws lambda update-function-code --function-name morning-edition --zip-file fileb://function.zip
