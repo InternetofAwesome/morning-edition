c26ec15c616f55f1478077494cd22f8b8e789b96
sam <me@samsurl.com>
2023-01-29 10:03:34 -0800

    update requirements

--- run.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-
-import function
-
-print(function.lambda_handler(0, 0))
-# print function.get_day_urls()
--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-aws lambda invoke --function-name morning-edition --log-type Tail  outfile
-cat outfile
-rm outfile
